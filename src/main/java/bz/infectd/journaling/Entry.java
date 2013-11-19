a53244389438b8104062dcb79f0e7e8f75d9be7f
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2013-11-19 23:29:11 +0100

    Add builder for entries

--- src/main/java/bz/infectd/journaling/Entry.java
+++ src/main/java/bz/infectd/journaling/Entry.java
@@ -1,16 +1,41 @@
 package bz.infectd.journaling;
 
+import bz.infectd.membership.Heartbeat;
+
 /**
  * @author Danilo Queiroz <dpenna.queiroz@gmail.com>
  */
 public class Entry<T> {
     private T content;
 
-    public Entry(T content) {
+    protected Entry(T content) {
         this.content = content;
     }
 
     public T unwrap() {
         return this.content;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.unwrap().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Entry) {
+            return this.unwrap().equals(((Entry<?>) obj).unwrap());
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Simple builder for entries
+     */
+    public static class Builder {
+        public static Entry<Heartbeat> createEntry(Heartbeat hb) {
+            return new Entry<Heartbeat>(hb);
+        }
     }
 }
