a66fb0ebf98c14c5943ea71af35a15fb7eda870a
Danilo Penna Queiroz <danilo@deliveryhero.com>
2013-11-22 15:46:42 +0100

    Add javadoc and logging

--- src/main/java/bz/infectd/communication/udp/Client.java
+++ src/main/java/bz/infectd/communication/udp/Client.java
@@ -1,6 +1,7 @@
 package bz.infectd.communication.udp;
 
 import static bz.infectd.application.EventLoopWrapper.systemEventLoop;
+import static org.slf4j.LoggerFactory.getLogger;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -13,40 +14,78 @@ import io.netty.handler.logging.LoggingHandler;
 
 import java.net.InetSocketAddress;
 
+import org.slf4j.Logger;
+
 import bz.infectd.communication.protocol.gossip.P2PProtocol.Gossip;
 import bz.infectd.communication.protocol.gossip.P2PProtocol.Gossip.Type;
 
 /**
+ * A client to sent UDP {@link Gossip} messages to a given destination
+ * 
  * @author Danilo Queiroz <dpenna.queiroz@gmail.com>
  */
 public class Client {
 
+    private static final String BROADCAST_ADDRESS = "255.255.255.255";
+    private static final Logger logger = getLogger(Client.class);
     private InetSocketAddress address;
 
+    /**
+     * Creates a client to broadcast {@link Gossip} messages over UDP.
+     * 
+     * @param port
+     *            The destination PORT to sent to.
+     */
+    public Client(int port) {
+        this.address = new InetSocketAddress(BROADCAST_ADDRESS, port);
+    }
+
+    /**
+     * Creates a client to sent a {@link Gossip} message to a given host:port
+     * 
+     * @param host
+     *            The host to send messages to
+     * @param port
+     *            The destination PORT.
+     */
     public Client(String host, int port) {
         this.address = new InetSocketAddress(host, port);
     }
 
-    public void send(Gossip message) throws Exception {
+    /**
+     * Sends the given message.
+     * 
+     * @param message
+     *            The message to be sent.
+     * @throws InterruptedException
+     */
+    public void send(Gossip message) throws InterruptedException {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.group(systemEventLoop()).channel(NioDatagramChannel.class)
                 .option(ChannelOption.SO_BROADCAST, false)
                 .handler(new LoggingHandler(Client.class, LogLevel.INFO));
 
+        logger.debug("Trying to send {} to {}", message, this.address);
         Channel ch = bootstrap.bind(0).sync().channel();
-
         ch.writeAndFlush(this.createDatagramPacket(message));
+        logger.info("Message {} sent to {}", message, this.address);
         ch.close();
     }
 
+    /**
+     * Creates a {@link DatagramPacket} from the given {@link Gossip} message
+     */
     private DatagramPacket createDatagramPacket(Gossip message) {
         ByteBuf buf = Unpooled.copiedBuffer(message.toByteArray());
         return new DatagramPacket(buf, this.address);
     }
-
-    public static void main(String[] args) throws Exception {
-        Client m = new Client("127.0.0.1", 7777);
-        Gossip msg = Gossip.newBuilder().setType(Type.HEARTBEAT).build();
-        m.send(msg);
+    
+    /**
+     * Just for ad-hoc tests
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Client c = new Client("127.0.0.1", 7777);
+        Gossip g = Gossip.newBuilder().setType(Type.HEARTBEAT).build();
+        c.send(g);
     }
 }
--- src/main/java/bz/infectd/communication/udp/Server.java
+++ src/main/java/bz/infectd/communication/udp/Server.java
@@ -1,40 +1,71 @@
 package bz.infectd.communication.udp;
 
 import static bz.infectd.application.EventLoopWrapper.systemEventLoop;
+import static org.slf4j.LoggerFactory.getLogger;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+
 import bz.infectd.communication.protocol.gossip.P2PProtocol.Gossip;
 
 /**
+ * UDP Server to receive {@link Gossip} messages.
+ * 
  * @author Danilo Queiroz <dpenna.queiroz@gmail.com>
  */
 public class Server extends SimpleChannelInboundHandler<DatagramPacket> {
 
+    private static final Logger logger = getLogger(Client.class);
     private final int port;
 
+    /**
+     * @param port
+     *            The port this server should listen to.
+     */
     public Server(int port) {
         this.port = port;
     }
 
+    /**
+     * Makes the server start listen to the incoming PORT
+     */
     public void listen() throws Exception {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.group(systemEventLoop()).channel(NioDatagramChannel.class).handler(this);
-        // TODO remove no await
-        bootstrap.bind(this.port).sync().channel().closeFuture().await();
+        logger.info("UDP server listen to port {}", this.port);
+        bootstrap.bind(this.port).sync().channel().closeFuture();
     }
 
+    /* (non-Javadoc)
+     * @see io.netty.channel.SimpleChannelInboundHandler#channelRead0(io.netty.channel.ChannelHandlerContext, java.lang.Object)
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket packet) throws Exception {
-        byte[] array = this.extractBytes(packet);
-        Gossip message = Gossip.parseFrom(array);
-        System.err.println(">>>>>>>>>>>>>>>>>>>>" + message.getType());
-        ctx.close();
+        this.messageReceived(ctx, packet);
     }
 
+    /**
+     * Receives the {@link DatagramPacket} from Netty.
+     */
+    protected void messageReceived(ChannelHandlerContext ctx, DatagramPacket packet)
+            throws Exception {
+        logger.debug("Message received: {}", packet);
+        byte[] array = this.extractBytes(packet);
+        Gossip message = Gossip.parseFrom(array);
+        logger.info("Message received: {}", message);
+        // TODO do something with the message
+    }
+
+    /**
+     * Extracts the byte[] content of the given packet
+     */
     private byte[] extractBytes(DatagramPacket packet) {
         ByteBuf buf = packet.content();
         final byte[] array;
@@ -49,7 +80,11 @@ public class Server extends SimpleChannelInboundHandler<DatagramPacket> {
         return array;
     }
 
+    /**
+     * Just for ad-hoc tests
+     */
     public static void main(String[] args) throws Exception {
         new Server(7777).listen();
+        systemEventLoop().awaitTermination(5, TimeUnit.MINUTES);
     }
 }
