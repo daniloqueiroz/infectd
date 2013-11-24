f3a5f667082bc083287a9cb1dfeca61ac7bb4a84
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2013-11-24 22:18:19 +0100

    Refactor Daemon class

--- src/main/java/bz/infectd/Application.java
+++ src/main/java/bz/infectd/Application.java
@@ -1,15 +1,40 @@
 package bz.infectd;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 /**
  * @author Danilo Queiroz <dpenna.queiroz@gmail.com>
- *
+ * 
  */
 public class Application {
 
-    public static void main(String[] args) throws UnknownHostException, InterruptedException {
-        Daemon.boot();
+    public static void main(String[] args) throws UnknownHostException, InterruptedException,
+            SocketException {
+        // TODO parse parameters
+        String hostname = getHostname();
+        Daemon daemon = new Daemon(hostname);
+        daemon.boot();
     }
 
+    private static String getHostname() throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface netIF = interfaces.nextElement();
+            if (!netIF.isLoopback() && netIF.isUp()) {
+                Enumeration<InetAddress> addresses = netIF.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (address instanceof Inet4Address) {
+                        return address.getHostAddress();
+                    }
+                }
+            }
+        }
+        return "localhost";
+    }
 }
--- src/main/java/bz/infectd/Daemon.java
+++ src/main/java/bz/infectd/Daemon.java
@@ -1,13 +1,17 @@
 package bz.infectd;
 
+import static bz.infectd.communication.gossip.MessageFactory.createMessage;
 import static bz.infectd.core.EventLoopWrapper.systemEventLoop;
+import static org.slf4j.LoggerFactory.getLogger;
 import io.netty.channel.EventLoopGroup;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+
 import bz.infectd.communication.gossip.GossipHandler;
+import bz.infectd.communication.gossip.protocol.Messages.Gossip;
+import bz.infectd.communication.gossip.udp.Client;
 import bz.infectd.communication.gossip.udp.Server;
 import bz.infectd.core.Clock;
 import bz.infectd.core.EntriesProcessor;
@@ -21,26 +25,68 @@ import bz.infectd.membership.MembershipBoard;
  * @author Danilo Queiroz <dpenna.queiroz@gmail.com>
  */
 public class Daemon {
-    
-    public static final int PORT = 4243;
+
+    public static final int PORT = 4242;
     private static final long GOSSIP_INTERVAL = 30;
+    private static final Logger logger = getLogger(Daemon.class);
+    private Journal journal;
+    private HeartbeatMonitor monitor;
+    private Server server;
+    private String hostname;
+    private Clock clock;
+    private int port;
+
+    protected Daemon(String hostname) {
+        this.hostname = hostname;
+        this.port = PORT;
+    }
 
     /**
      * Boots the Infectd Daemon
      */
-    public static void boot() throws InterruptedException, UnknownHostException {
+    public void boot() throws InterruptedException {
+        logger.info("Starting daemon - {}:{}", this.hostname, this.port);
+        this.journal = this.setupJornal();
+        this.monitor = new HeartbeatMonitor(new Heartbeat(this.hostname, this.port));
+        this.server = this.setupServer();
+        this.server.listen();
+        this.broadcastHeartbeat();
+    }
+
+    private Journal setupJornal() {
         MembershipBoard board = new MembershipBoard();
         EntriesProcessor processor = new EntriesProcessor(board);
-        Journal journal = new Journal(processor);
-        GossipHandler handler = new GossipToEntryAdapter(journal);
-        Server udpServer = new Server(PORT, handler);
-        udpServer.listen();
-        // TODO get public address
-        Heartbeat hb = new Heartbeat(InetAddress.getLocalHost().getHostAddress(), PORT);
-        HeartbeatMonitor monitor = new HeartbeatMonitor(hb);
-        // Announce itself
-        Clock clock = new Clock(journal, monitor);
-        EventLoopGroup eventLoop = systemEventLoop();
-        eventLoop.scheduleWithFixedDelay(clock, 10, GOSSIP_INTERVAL, TimeUnit.SECONDS);
+        return new Journal(processor);
+    }
+
+    private Server setupServer() {
+        final GossipHandler handler = new GossipToEntryAdapter(this.journal);
+        Server udpServer = new Server(PORT, new GossipHandler() {
+            private transient boolean hasReceivedFirstMessage = false;
+
+            @Override
+            public void addEntry(Gossip message) {
+                if (!this.hasReceivedFirstMessage) {
+                    // Not thread-safe, but we rely on the setupClock that is
+                    this.hasReceivedFirstMessage = true;
+                    Daemon.this.setupClock();
+                }
+                handler.addEntry(message);
+            }
+        });
+        return udpServer;
+    }
+
+    private synchronized void setupClock() {
+        if (this.clock == null) {
+            this.clock = new Clock(this.journal, this.monitor);
+            EventLoopGroup eventLoop = systemEventLoop();
+            eventLoop.scheduleWithFixedDelay(this.clock, 10, GOSSIP_INTERVAL, TimeUnit.SECONDS);
+        }
+    }
+
+    private void broadcastHeartbeat() throws InterruptedException {
+        Client udpClient = new Client(this.port);
+        udpClient.send(createMessage(this.monitor.heartbeat()));
     }
 }
