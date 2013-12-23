c653437625c1fdb567c02fab05e1ee409a3b6a13
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2013-12-24 00:03:30 +0100

    Base32 encode key when writing/reading to/from file

--- src/main/java/bz/infectd/crypto/SecretKeyUtils.java
+++ src/main/java/bz/infectd/crypto/SecretKeyUtils.java
@@ -5,10 +5,13 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import com.google.common.io.BaseEncoding;
 
 /**
  * @author Danilo Queiroz <dpenna.queiroz@gmail.com>
@@ -16,23 +19,32 @@ import javax.crypto.spec.SecretKeySpec;
 public class SecretKeyUtils {
 
     private static final String CRYPTO_ALGORITHM = "AES";
+    private static final int KEY_SIZE = 256;
+    private static KeyGenerator generator;
+    private static BaseEncoding base32 = BaseEncoding.base32();
 
-    public static SecretKey generate() {
+    static {
         try {
-            return KeyGenerator.getInstance(CRYPTO_ALGORITHM).generateKey();
+            generator = KeyGenerator.getInstance(CRYPTO_ALGORITHM);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+        generator.init(KEY_SIZE, new SecureRandom());
+    }
+
+    public static SecretKey generate() {
+        return generator.generateKey();
     }
 
     public static void writeKey(SecretKey key, WritableByteChannel chn) throws IOException {
-        ByteBuffer buf = ByteBuffer.wrap(key.getEncoded());
+        ByteBuffer buf = ByteBuffer.wrap(base32.encode(key.getEncoded()).getBytes());
         chn.write(buf);
     }
 
     public static SecretKey loadKey(ReadableByteChannel chn, int keySize) throws IOException {
         ByteBuffer buf = ByteBuffer.allocate(keySize);
         chn.read(buf);
-        return new SecretKeySpec(buf.array(), CRYPTO_ALGORITHM);
+        byte[] bytes = base32.decode(new String(buf.array()));
+        return new SecretKeySpec(bytes, CRYPTO_ALGORITHM);
     }
 }
